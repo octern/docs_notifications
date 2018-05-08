f84eefa47e0006097730f86b89c287392f1b1774
Ben <benspaul@gmail.com>
2018-05-07 20:15:33 -0700

    creates triggers itself, no longer attached to sheet, some cleaning

--- docs_notifications.js
+++ docs_notifications.js
@@ -18,29 +18,32 @@ Change Log:
 
 May-01-2015: Updated  file.getEditors[].getEmail() method call.
 Oct-21-2015: V2 Released - Name of the Last Modified User will be sent along with the Timestamp.
-Apr-22-2018: Ben Paul modified this for Indivisible SF
-Apr-30-2018: Michael Cohn modified to check all docs inside a folder
+Apr-22-2018: Ben Paul modified this for Indivisible SF.
+Apr-30-2018: Michael Cohn modified to check all docs inside a folder.
+May-07-2018: Ben Paul modified this to create triggers itself and not to be attached to a sheet.
 
 INSTRUCTIONS:
-* Open the spreadsheet this script is attached to and put the ID for the google drive folder to monitor in the designated space.
-* Example spreadsheet: https://docs.google.com/spreadsheets/d/1wpG_mY9CAon-E8gHK-Dm8Iw8D6rhA07V8BaviZ7xIcM/edit#gid=0
-* Edit the variable "identifier" to specify a string that appears at the beginning of every file to monitor. (Use "" to monitor all files)
-* Set a trigger for the script to run regularly
+* Edit the variable "identifier" to specify a string that is contained in every file to monitor. Case-insensitive. (Use "" to monitor all files.)
+* Run main(). This will create a trigger for the script to run regularly.
 */
 
 function main() {
-
-  var sheet = SpreadsheetApp.getActiveSheet();
-  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  var identifier = "Accepting Comments";
-  var folderId = sheet.getRange("C6").getValue();
-  var folderId = folderId.toString().trim();
-  var folder = DriveApp.getFolderById(folderId);
-  var allFiles = folder.getFiles();
-  processFiles(allFiles, identifier);
+  clearTriggers();
+  ScriptApp.newTrigger("processFiles").timeBased().everyMinutes(1).create();
 }
 
-function processFiles(files, identifier) {
+function clearTriggers() {
+    var allTriggers = ScriptApp.getProjectTriggers();
+    for (var i = 0; i < allTriggers.length; i++)
+        ScriptApp.deleteTrigger(allTriggers[i]);
+}
+
+function processFiles() {
+  var identifier = "accepting comments";
+  var folderId = "1630EKTJErgblzc5oeDQNiK6riTqFweUP";
+  var folder = DriveApp.getFolderById(folderId);
+  var files = folder.getFiles();
+  
   while (files.hasNext()) {
     var file = files.next();
     var filename = file.getName();
@@ -51,23 +54,15 @@ function processFiles(files, identifier) {
       monitorFile(fileId);
     }
  }
-
 }
 
-
-function getFile_(fileId)
-{
-    var file = DriveApp.getFileById(fileId);
-    return file;
-}
-
-function getDoc_(fileId)
+function getDoc(fileId)
 {
    var doc = DocumentApp.openById(fileId);
    return doc;
 }
 
-function getUserKey_(fileId) {
+function getUserKey(fileId) {
   // this is where the last version of document comments is saved in your user profile (the user running the script)
   var userKey = "Proposal " + fileId + " Comments Text";
   return userKey;
@@ -79,36 +74,38 @@ function monitorFile(fileId) {
   try {
     var file = DriveApp.getFileById(fileId);
     var userProperties = PropertiesService.getUserProperties();
-    var userKey = getUserKey_(fileId);
+    var userKey = getUserKey(fileId);
     var previousComments = userProperties.getProperty(userKey);
     var comments = getComments(fileId);
 
     if (comments != previousComments || previousComments == null) {
       Logger.log("Modified");
       var emails = getEmails(fileId);
+      Logger.log("Found emails " + emails);
 
       if(previousComments == null) {
-          var emailSubject = "New proposal created: " + file.getName();
+          var fileName = file.getName();
+          var emailSubject = "New proposal created: " + fileName;
           var docLink = "https://docs.google.com/document/d/" + fileId;
-          var emailBody = emailSubject + "\n\n" +
-            "A new proposal was created at \n\n" +
+          var emailBody = "A new proposal, " + fileName + ", was created at\n\n" +
                 docLink + "\n\n" +
-                  "You will receive email notifications about comments that are added.";
+                  "You will receive email notifications about comments that are added. To unsubscribe, remove your email from the proposal in the Email Subscriptions section.";
       }
       else {
-          var emailSubject = "New Comment on " + file.getName();
+          var fileName = file.getName();
+          var emailSubject = "New comment on " + fileName;
           var docLink = "https://docs.google.com/document/d/" + fileId;
-          var emailBody = emailSubject + "\n\n" +
-            "Here are all the comments:" + "\n\n" +
-              comments + "\n\n" +
+          var emailBody = "There's a new comment on " + fileName + ".\n\n" +
                 docLink + "\n\n" +
-                  "You are receiving this notification because your email is listed on the proposal under the Get Emails section. To unsubscribe, remove your email from the proposal.";
+                  "Here are all the comments:" + "\n\n" +
+                    comments + "\n\n" +
+                      "You are receiving this notification because your email is listed on the proposal. To unsubscribe, remove your email from the proposal in the Email Subscriptions section.";
       }
       if(emails && emails.length >= 1) {
-        Logger.log("attempting to email " + emails.join());
+        Logger.log("Attempting to email " + emails.join());
         for (var i = 0; i < emails.length; i++) {
           MailApp.sendEmail(emails[i], emailSubject, emailBody);
-          Logger.log("email sent to " + emails[i]);
+          Logger.log("Email sent to " + emails[i]);
         }
 
       }
@@ -129,11 +126,11 @@ function getEmails(fileId) {
     // email regex from stackoverflow
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    var doc = getDoc_(fileId);
+    var doc = getDoc(fileId);
     var body = doc.getBody();
     var text = body.getText();
-    var start = text.indexOf("[Start Emails]");
-    var end = text.indexOf("[End Emails]");
+    var start = text.toLowerCase().indexOf("[start emails]");
+    var end = text.toLowerCase().indexOf("[end emails]");
     var match = text.substring(start, end);
     match = match.replace(/,/g, "\n"); // in case of a comma delimited list, replace comma with new line
     var lines = match.split('\n');
@@ -150,27 +147,12 @@ function getEmails(fileId) {
 }
 
 function getComments(fileId) {
-  var doc = getDoc_(fileId);
+  var comments_identifier = "comments (anyone can write one)"
+  var doc = getDoc(fileId);
   var body = doc.getBody();
   var text = body.getText();
-  var start = text.indexOf("Comments (anyone can write one)");
+  var start = text.toLowerCase().indexOf(comments_identifier) + comments_identifier.length;
   var match = text.substring(start).trim(); // go until the end of the doc
 
   return match;
-}
-
-function authorize()
-{
-  spreadsheet.toast("Once authorized, enter File ID, Email IDs and select Google Doc Monitor-> Start Monitoring","",20);
-}
-
-function onOpen()
-{
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var menuEntries = [ {name: "1. Authorize", functionName: "authorize"},
-    {name: "2. Start Monitoring", functionName: "startMonitoring"},
-    {name: "3. Stop Monitoring", functionName: "stopMonitoring"},
-  ];
-  ss.addMenu("➤ Google Doc Monitor", menuEntries);
-  spreadsheet.toast("Once Drive API has been enabled, Select Google Doc Monitor-> Authorize. This is an One Time Action.","Get Started",-1);
 }
